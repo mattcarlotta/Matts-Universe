159f86319357e34e4f81c1594d25abc80d394616
Matt <carlotta.matt@gmail.com>
2018-10-20 18:55:46 -0700

    Fix styles

--- client/src/components/Blog/RenderPagination/renderPagination.js
+++ client/src/components/Blog/RenderPagination/renderPagination.js
@@ -15,34 +15,32 @@ class Pagination extends PureComponent {
     const { currentPage, pageCount, postCount } = this.props;
     return (
       <ul className={paginationContainer}>
-        <li className={currentPage === 1 ? `${disableChevron}` : null}>
+        <li>
           <button
             type="button"
+            className={currentPage === 1 ? `${disableChevron}` : null}
             onClick={() => this.handlePaginationClick(currentPage - 1)}
           >
             <span className="small-font">&#60;</span>
           </button>
         </li>
         {map(pageCount, page => (
-          <li
-            key={page}
-            className={currentPage === page + 1 ? `${activePage}` : null}
-          >
+          <li key={page}>
             <button
               type="button"
+              className={currentPage === page + 1 ? `${activePage}` : null}
               onClick={() => this.handlePaginationClick(page + 1)}
             >
               {page + 1}
             </button>
           </li>
         ))}
-        <li
-          className={
-            (currentPage + 1) * 10 <= postCount ? null : `${disableChevron}`
-          }
-        >
+        <li>
           <button
             type="button"
+            className={
+              (currentPage + 1) * 10 <= postCount ? null : `${disableChevron}`
+            }
             onClick={() => this.handlePaginationClick(currentPage + 1)}
           >
             <span style={{ fontSize: 10 }}>&#62;</span>
