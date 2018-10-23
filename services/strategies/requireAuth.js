172175c9be451e4d000f0a74e734ecc46bd70e43
Matt <carlotta.matt@gmail.com>
2018-10-22 23:42:33 -0700

    Fix requireAuth

--- services/strategies/requireAuth.js
+++ services/strategies/requireAuth.js
@@ -2,7 +2,7 @@ module.exports = app => (req, res, next) => {
   const isEmpty = app.get('isEmpty');
   const { badCredentials } = app.shared.authErrors;
 
-  return isEmpty(req.session)
+  return isEmpty(req.session) || !req.session.god
     ? res.status(401).send({ err: badCredentials })
     : next();
 };
